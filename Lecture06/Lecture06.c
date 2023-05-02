52d9fd71b69ab29d4aea91f402a0fe4137c04833
rukering <mecata98@gmail.com>
2023-05-02 11:44:34 +0900

    commit to file complited

--- Lecture06/Lecture06.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <RunningDash_Screen.h>	
-int main()
-{
-	system("mkdir Cardinal");
-	return 0;
-}
--- Lecture06/RunningDash_Screen.h
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int Print_Title_Screen();
-
-int Print_How_To_Play_Screen();
-
